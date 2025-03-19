61f93fc036f5aa6dfcc5b238fb2b564e05b17d9b
PhucGrut <ngotronghl52@gmail.com>
2025-03-19 16:02:49 +0700

    up product

--- Domain/Data/Entities/Product.cs
+++ Domain/Data/Entities/Product.cs
@@ -10,16 +10,16 @@ namespace Domain.Data.Entities
     public class Product : BaseEntity
     {
         public Guid ProductId { get; set; }
-        public int? CategoryId { get; set; }
-        public int? BrandId { get; set; }
-        public string ProductName { get; set; } 
-        public string? Description { get; set; }
-        public string? PrivewImage { get; set; }
-        public string? Origin { get; set; }
-
         public string? ProductCode { get; set; }
+        public string ProductName { get; set; } 
+        public string? PrivewImage { get; set; }
         public decimal? Price { get; set; }
         public int? Stock { get; set; }
+        public string? Origin { get; set; }
+        public string? Description { get; set; }
+        public bool? HasVariations { get; set; }
+        public int? CategoryId { get; set; }
+        public int? BrandId { get; set; }
 
         //public string?  { get; set; }
         public Category? Category { get; set; }
