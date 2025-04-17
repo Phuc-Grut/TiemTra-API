b2fb171b97bc99ef1cc0121383417ee44af2118c
PhucGrut <ngotronghl52@gmail.com>
2025-04-17 23:46:33 +0700

    up set attribute for category, select attribute

---
+++ src/Application/DTOs/Attributes/SetAttributesForCategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Attributes
+{
+    public class SetAttributesForCategoryDTO
+    {
+        public int CategoryId { get; set; }
+        public List<int> AttributeIds { get; set; } = new();
+    }
+}
--- src/Infrastructure/Interface/ICategoryAttributesRepository.cs
+++ src/Infrastructure/Interface/ICategoryAttributesRepository.cs
@@ -4,10 +4,11 @@ namespace Infrastructure.Interface
 {
     public interface ICategoryAttributesRepository
     {
-        Task AddAttributesToCategory(CategoryAttributes categoryAttribute, CancellationToken cancellationToken);
+        Task<List<int>> GetAttributeIdsByCategory(int categoryId, CancellationToken cancellationToken);
 
         Task RemoveAllAttributesFromCategory(int categoryId, CancellationToken cancellationToken);
-
+        Task RemoveAsync(int categoryId, List<int> attributeIds, CancellationToken cancellationToken);
+        Task AddAsync(CategoryAttributes categoryAttribute, CancellationToken cancellationToken);
         Task<List<Attributes>> GetAttributesByCategory(int categoryId, CancellationToken cancellationToken);
 
         Task<bool> ExistsAsync(int categoryId, int attributeId);
