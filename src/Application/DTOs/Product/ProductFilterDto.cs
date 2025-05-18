3e231908cda19bfd216f61dd67f0067da13c7b18
PhucGrut <ngotronghl52@gmail.com>
2025-05-18 23:26:13 +0700

    add TotalSold

--- src/Application/DTOs/Product/ProductFilterDto.cs
+++ src/Application/DTOs/Product/ProductFilterDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,8 @@ namespace Application.DTOs.Product
     {
         public string? ProductCode { get; set; }
         public string? Keyword { get; set; }
+        public ProductStatus? Status { get; set; }
+        public string? SortBy { get; set; }
+        public int? CategoryId { get; set; }
     }
 }
