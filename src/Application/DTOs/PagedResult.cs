cbe4fcf7f6fcb7ab80675942fddede1b040e9109
PhucGrut <ngotronghl52@gmail.com>
2025-05-19 21:40:42 +0700

    up

--- src/Application/DTOs/PagedResult.cs
+++ src/Application/DTOs/PagedResult.cs
@@ -7,6 +7,5 @@
         public int TotalPages { get; set; }
         public int CurrentPage { get; set; }
         public int PageSize { get; set; }
-        public int? BrandId { get; set; }
     }
 }
