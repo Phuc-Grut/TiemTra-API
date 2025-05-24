7c7e078903f098f97141efb0a5aeeb27e8d1fe49
PhucGrut <ngotronghl52@gmail.com>
2025-05-24 17:21:24 +0700

    up api create sản phẩm

--- src/Application/DTOs/Product/CreateProductDTO.cs
+++ src/Application/DTOs/Product/CreateProductDTO.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Enum;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,9 @@ namespace Application.DTOs.Product
         public bool HasVariations { get; set; }
         public int CategoryId { get; set; }
         public int? BrandId { get; set; }
+        public int? TotalSold { get; set; }
+        public string? Note { get; set; }
+        public ProductStatus ProductStatus { get; set; }
 
         public List<string> ProductImageUrls { get; set; } = new();
 
