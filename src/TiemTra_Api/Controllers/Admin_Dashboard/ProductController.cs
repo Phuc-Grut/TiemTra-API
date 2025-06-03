06f22664ee23a29616ccc4a9eca465c159920d1c
PhucGrut <ngotronghl52@gmail.com>
2025-06-03 15:13:32 +0700

    api update product

--- src/TiemTra_Api/Controllers/Admin_Dashboard/ProductController.cs
+++ src/TiemTra_Api/Controllers/Admin_Dashboard/ProductController.cs
@@ -11,11 +11,11 @@ namespace TiemTra_Api.Controllers.Admin_Dashboard
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly IProductServices _productService;
+        private readonly IProductServices _productServices;
         private readonly IFileStorageService _fileStorageService;
         public ProductController(IProductServices productServices, IFileStorageService fileStorageService)
         {
-            _productService = productServices;
+            _productServices = productServices;
             _fileStorageService = fileStorageService;
         }
 
@@ -27,7 +27,7 @@ namespace TiemTra_Api.Controllers.Admin_Dashboard
 
             var user = User;
 
-            var result = await _productService.CreateProductAsync(dto, user, cancellationToken);
+            var result = await _productServices.CreateProductAsync(dto, user, cancellationToken);
 
             if (!result)
             {
@@ -53,7 +53,7 @@ namespace TiemTra_Api.Controllers.Admin_Dashboard
         [HttpGet("generate-product-code")]
         public async Task<IActionResult> GenerateProductCode(CancellationToken cancellationToken)
         {
-            var productCode = await _productService.GenerateUniqueProductCodeAsync();
+            var productCode = await _productServices.GenerateUniqueProductCodeAsync();
             return Ok(productCode);
         }
 
@@ -64,7 +64,7 @@ namespace TiemTra_Api.Controllers.Admin_Dashboard
             [FromQuery] int pageSize = 5,
             CancellationToken cancellationToken = default)
         {
-            var result = await _productService.GetPagingAsync(filterDto, pageNumber, pageSize, cancellationToken);
+            var result = await _productServices.GetPagingAsync(filterDto, pageNumber, pageSize, cancellationToken);
             return Ok(result);
         }
 
@@ -74,11 +74,30 @@ namespace TiemTra_Api.Controllers.Admin_Dashboard
             if (productId == Guid.Empty)
                 return BadRequest("Có lỗi khi lấy dữ liệu");
 
-            var product = await _productService.GetProductByIdAsync(productId, cancellationToken);
+            var product = await _productServices.GetProductByIdAsync(productId, cancellationToken);
             if (product == null)
                 return NotFound("Sản phẩm không tồn tại");
 
             return Ok(product);
+        }
+
+
+        [HttpPut("update-product/{productId}")]
+        public async Task<IActionResult> Update(Guid productId, [FromBody] CreateProductDto dto, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _productServices.UpdateProductAsync(productId, User, dto, cancellationToken);
+
+                if (result)
+                    return Ok(new { success = true, message = "Cập nhật sản phẩm thành công" });
+
+                return BadRequest(new { success = false, message = "Cập nhật sản phẩm thất bại" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
         }
     }
 }
