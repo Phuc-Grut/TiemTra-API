d942f5a0ce9e5ce4fd937daf683873a255c49fc7
PhucGrut <ngotronghl52@gmail.com>
2025-06-12 13:51:09 +0700

    fix Register

--- src/Application/DTOs/Admin/Product/ProductVariationDTO.cs
+++ src/Application/DTOs/Admin/Product/ProductVariationDTO.cs
@@ -8,7 +8,7 @@ namespace Application.DTOs.Admin.Product
 {
     public class ProductVariationDto
     {
-        //public Guid ProductVariationId { get; set; }
+        public Guid? ProductVariationId { get; set; }
         public string TypeName { get; set; }
         public decimal Price { get; set; }
         public int? Stock { get; set; }
