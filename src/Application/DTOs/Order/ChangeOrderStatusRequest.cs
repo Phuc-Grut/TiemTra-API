b028c47c725ab2e0748c6f96a8f93914d5d23254
PhucGrut <ngotronghl52@gmail.com>
2025-07-14 22:58:50 +0700

    api ChangeOrderStatus

---
+++ src/Application/DTOs/Order/ChangeOrderStatusRequest.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Order
+{
+    public class ChangeOrderStatusRequest
+    {
+        public OrderStatus NewStatus { get; set; }
+    }
+}
