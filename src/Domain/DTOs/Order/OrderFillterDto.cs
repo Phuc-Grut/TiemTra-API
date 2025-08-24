7397fb952357365ed4af303eb5e28e4f2a9312ea
PhucGrut <ngotronghl52@gmail.com>
2025-08-24 22:59:33 +0700

    sort order

--- src/Domain/DTOs/Order/OrderFillterDto.cs
+++ src/Domain/DTOs/Order/OrderFillterDto.cs
@@ -13,9 +13,10 @@ namespace Domain.DTOs.Order
         public string? CustomerCode { get; set; }
         public OrderStatus OrderStatus { get; set; }
         public DateTime? CreateAt { get; set; }
-        public DateTime? UpdateAt { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
         public PaymentStatus PaymentStatus { get; set; }
+
+        public string? SortBy { get; set; }
 
     }
 }
